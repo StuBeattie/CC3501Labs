588882af4c33cdd97edf8ea5752bcff5476d024e
StuBeattie <79081146+StuBeattie@users.noreply.github.com>
2024-08-07 21:41:56 +1000

    SocketsDemo

---
+++ SocketsDemo/build/CMakeFiles/udp_receive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udp_receive.dir/src/udp_receive.cpp.o"
+  "udp_receive"
+  "udp_receive.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udp_receive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SocketsDemo/build/CMakeFiles/udp_transmit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udp_transmit.dir/src/udp_transmit.cpp.o"
+  "udp_transmit"
+  "udp_transmit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udp_transmit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
